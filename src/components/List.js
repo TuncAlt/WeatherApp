b228bc23629dcba09f1906255f74c0671120d0e8
Tuncer Altun <tunc.alt@gmail.com>
2023-02-14 17:54:05 +0100

    Add Filter

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,24 @@
 //import useLocalStorageState from "use-local-storage-state"
 
-export function List({activities}) {
-    const listItems = activities.map((activity) => {
-        return  <li key={activity.id} 
-        goodweather={activity.goodweather}
-        >{activity.name} - is good for outdoor: {activity.goodweather ? "Yes" : "No"}</li>
-    })
-  
-return (
-        <div>
-            <ul>{listItems}</ul>
-        </div>
-)}
+export function List({ activities, isGoodWeather }) {
+  const listItems = activities.map((activity) => {
+    return (
+      <li key={activity.id} goodweather={activity.goodweather}>
+        {activity.name} - is good for outdoor:{" "}
+        {activity.goodweather ? "Yes" : "No"}
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      {isGoodWeather ? (
+        <h2>Good Weather Activity</h2>
+      ) : (
+        <h2>Bad Weather Activity</h2>
+      )}
+
+      <ul>{listItems}</ul>
+    </div>
+  );
+}
